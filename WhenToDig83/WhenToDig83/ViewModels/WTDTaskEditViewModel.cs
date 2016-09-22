9ddf2311079d6ef68e00882458de27af3a9bbdb1
ivNetAdmin <bp@ivnet.co.uk>
2016-09-22 16:00:38 +0100

    WTDTaskViewModel

---
+++ WhenToDig83/WhenToDig83/ViewModels/WTDTaskEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhenToDig83.ViewModels
+{
+    class WTDTaskEditViewModel
+    {
+    }
+}
