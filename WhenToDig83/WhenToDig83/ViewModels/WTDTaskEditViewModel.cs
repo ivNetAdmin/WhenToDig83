ec051d934101bd74d33a85e5f74df406f6b1e927
ivNetAdmin <bp@ivnet.co.uk>
2016-09-22 16:07:06 +0100

    Update WTDTaskEditViewModel.cs

--- WhenToDig83/WhenToDig83/ViewModels/WTDTaskEditViewModel.cs
+++ WhenToDig83/WhenToDig83/ViewModels/WTDTaskEditViewModel.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace WhenToDig83.ViewModels
 {
-    class WTDTaskEditViewModel
+    public class WTDTaskEditViewModel : INotifyPropertyChanged, IPageLifeCycleEvents
     {
+        private INavigation _navigation;
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public WTDTaskEditViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+            
+            //var wtdTaskManager = new WTDTaskManager();
+
+            //var tasks = wtdTaskManager.GetTasksByMonth(DateTime.Now.Month, DateTime.Now.Year);
+
+            //WTDTasks = new ObservableCollection<WTDTask>(tasks);
+        }
+        
+        #region Properties
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(() => Name);
+            }
+        }
+        
+        private DateTime _date;
+        public DateTime Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                RaisePropertyChanged(() => Date);
+            }
+        }
+        
+        private string _type;
+        public string Type
+        {
+            get { return _type; }
+            set
+            {
+                _type = value;
+                RaisePropertyChanged(() => Type);
+            }
+        }
+
+        private string _notes;
+        public string Notes
+        {
+            get { return _notes; }
+            set
+            {
+                _notes = value;
+                RaisePropertyChanged(() => Notes);
+            }
+        }
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        
+        #region Page Events
+        public void OnAppearing()
+        {
+
+        }
+        #endregion
     }
 }
