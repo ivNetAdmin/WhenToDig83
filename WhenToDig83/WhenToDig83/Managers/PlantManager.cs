2959be88d2b9fb55c83e3e20e57592ff07a2442d
ivNetAdmin <bp@ivnet.co.uk>
2016-09-26 15:43:11 +0100

    Update PlantManager.cs

--- WhenToDig83/WhenToDig83/Managers/PlantManager.cs
+++ WhenToDig83/WhenToDig83/Managers/PlantManager.cs
@@ -1,12 +1,30 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using WhenToDig83.Core.Entities;
+using WhenToDig83.Core.Enums;
+using WhenToDig83.Data;
 
 namespace WhenToDig83.Managers
 {
     public class PlantManager
     {
+        private RepositoryAsync<Plant> _plantRepository;
+
+        public PlantManager()
+        {
+            _plantRepository = new RepositoryAsync<Plant>();
+        }
+        
+        public async void AddPlant(string name)
+        {
+            await _plantRepository.Insert(new Plant { Name = name });
+        }
+        
+        public async List<Plant> GetPlants()
+        { 
+            return await _plantRepository.Get();
+        }
     }
 }
