a78d71516a2f40693f268dfa2d4ee8216c972f62
ivNetAdmin <bp@ivnet.co.uk>
2016-09-26 15:05:45 +0100

    Update PlantViewModel.cs

--- WhenToDig83/WhenToDig83/ViewModels/PlantViewModel.cs
+++ WhenToDig83/WhenToDig83/ViewModels/PlantViewModel.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WhenToDig83.Core.Entities;
+using WhenToDig83.Helpers;
+using WhenToDig83.Managers;
+using WhenToDig83.Pages;
+using Xamarin.Forms;
 
 namespace WhenToDig83.ViewModels
-{
-    class PlantViewModel
+{    
+    internal class PlantViewModel : BaseModel
     {
+        private INavigation _navigation;
+        private PlantManager _plantManager;
+    
+        public PlantViewModel()
+        {
+            _plantManager = new PlantManager();
+        }
+        
+        
+        #region Page Events
+        protected override void CurrentPageOnAppearing(object sender, EventArgs eventArgs)
+        {
+            try
+            {
+                _navigation = AppHelper.CurrentPage().Navigation;               
+            }
+            catch (Exception exception)
+            {
+                ResponseText = exception.ToString();
+            }
+        }
+        #endregion
+        
+        #region Navigation Events
+        public ICommand ToolbarNavigationCommand
+        {
+            get
+            {
+                return new Command<string>(async (string paramter) =>
+                {
+                    switch(paramter)
+                    {
+                        case "task":
+                            await _navigation.PushModalAsync(new WTDTaskPage());
+                            break;                        
+                    }
+                   
+                });
+            }
+        }
+        #endregion
     }
 }
