a2733022814112390bf56f75e61b2728aa3c3727
ivNetAdmin <bp@ivnet.co.uk>
2016-10-03 16:39:16 +0100

    Update ReviewManager.cs

--- WhenToDig83/WhenToDig83/Managers/ReviewManager.cs
+++ WhenToDig83/WhenToDig83/Managers/ReviewManager.cs
@@ -8,5 +8,11 @@ namespace WhenToDig83.Managers
 {
     public class ReviewManager
     {
+        private RepositoryAsync<Note> _noteRepository;
+
+        public ReviewManager()
+        {
+            _noteRepository = new RepositoryAsync<Note>();
+        }
     }
 }
