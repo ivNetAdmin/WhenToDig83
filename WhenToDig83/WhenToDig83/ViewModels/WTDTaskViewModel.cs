206d4dc89be0108b8cfc57f10b88f8d39e30ab59
ivNetAdmin <bp@ivnet.co.uk>
2016-10-06 16:52:14 +0100

    Update WTDTaskViewModel.cs

--- WhenToDig83/WhenToDig83/ViewModels/WTDTaskViewModel.cs
+++ WhenToDig83/WhenToDig83/ViewModels/WTDTaskViewModel.cs
@@ -151,6 +151,9 @@ namespace WhenToDig83.ViewModels
                         case "review":
                             await _navigation.PushAsync(new ReviewPage());
                             break;
+                        case "frost":
+                            await _navigation.PushAsync(new FrostPage());
+                            break;
                     }
                    
                 });
