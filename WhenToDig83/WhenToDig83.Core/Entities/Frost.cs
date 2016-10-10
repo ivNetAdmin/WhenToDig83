cb472bd115dd34e198181cf01a786d0396d65335
ivNetAdmin <bp@ivnet.co.uk>
2016-10-10 13:57:05 +0100

    Update Frost.cs

--- WhenToDig83/WhenToDig83.Core/Entities/Frost.cs
+++ WhenToDig83/WhenToDig83.Core/Entities/Frost.cs
@@ -9,6 +9,7 @@ namespace WhenToDig83.Core.Entities
         public int Day { get; set; }
         public int Month { get; set; }
         public int Year { get; set; }
-        public string Date { get; set; }      
+        public string Date { get; set; }  
+        public int count { get; set; }
     }
 }
